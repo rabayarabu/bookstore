87a486cb7a85197a78a8879317f02362a1b9fe35
rabayarabu <rabu9149@gmail.com>
2023-06-22 22:52:03 +0600

    Fixed changes required

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -13,41 +13,39 @@ const BookList = (prop) => {
   };
 
   return (
-    <li>
-      <div className="booklist">
-        <div className="book">
-          <p>
-            {category}
-          </p>
-          <h2>
-            {title}
-          </h2>
-          <p>
-            {author}
-          </p>
-          <div className="buttonlist">
-            <button type="button" className="btn">Comments</button>
-            <button type="button" className="btn" onClick={handleDelete}>Remove</button>
-            <button type="button" className="btn">Edit</button>
-          </div>
-        </div>
-        <div className="progress blue">
-          <span className="progress-left">
-            <span className="progress-bar" />
-          </span>
-          <span className="progress-right">
-            <span className="progress-bar" />
-          </span>
-          <div className="progress-value">90%</div>
-          <div className="completed">Completed</div>
-        </div>
-        <div className="chapter">
-          <h5 className="current-chapter">CURRENT CHAPTER</h5>
-          <p>Chapter 1</p>
-          <button type="button" className="progress-btn">UPDATE PROGRESS</button>
+    <div className="booklist">
+      <div className="book">
+        <p>
+          {category}
+        </p>
+        <h2>
+          {title}
+        </h2>
+        <p>
+          {author}
+        </p>
+        <div className="buttonlist">
+          <button type="button" className="btn">Comments</button>
+          <button type="button" className="btn" onClick={handleDelete}>Remove</button>
+          <button type="button" className="btn">Edit</button>
         </div>
       </div>
-    </li>
+      <div className="progress blue">
+        <span className="progress-left">
+          <span className="progress-bar" />
+        </span>
+        <span className="progress-right">
+          <span className="progress-bar" />
+        </span>
+        <div className="progress-value">90%</div>
+        <div className="completed">Completed</div>
+      </div>
+      <div className="chapter">
+        <h5 className="current-chapter">CURRENT CHAPTER</h5>
+        <p>Chapter 1</p>
+        <button type="button" className="progress-btn">UPDATE PROGRESS</button>
+      </div>
+    </div>
 
   );
 };
